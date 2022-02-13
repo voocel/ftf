d9a121ef08d710e88099870d3115b77403abb9af
voocel <voocel@gmail.com>
2022-02-13 23:41:00 +0800

    wip: add examples

---
+++ examples/server/server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/voocel/ftf/network"
+)
+
+func main() {
+	l := logrus.New()
+	l.SetLevel(logrus.DebugLevel)
+	l.SetFormatter(&logrus.JSONFormatter{})
+	s := network.NewServer("0.0.0.0:1235", network.WithHeartbeat(5*time.Second), network.WithLogger(l))
+
+	s.OnConnect(func(c *network.Client) {
+		l.Println("connected by: %v\n", c.GetClientIP())
+		go input(c)
+	})
+
+	s.OnMessage(func(c *network.Client, msg *network.Message) {
+		fmt.Println(msg)
+	})
+
+	s.OnClose(func(c *network.Client, err error) {
+		l.Printf("closed by[%v]: %v\n", c.GetClientIP(), err)
+		ch := make(chan struct{}, 1)
+		ch <- struct{}{}
+		c.SetExtraMap(c.GetClientIP().String(), ch)
+	})
+
+	s.Start()
+}
+
+func input(c *network.Client) {
+	for {
+		ch := c.GetExtraMap(c.GetClientIP().String())
+		if ch != nil {
+			cc := ch.(chan struct{})
+			select {
+			case <-cc:
+				return
+			default:
+			}
+		}
+
+		reader := bufio.NewReader(os.Stdin)
+		s, _ := reader.ReadString('\n')
+		s = strings.Trim(s, "\n")
+		c.SendBytes(network.Heartbeat, []byte(s))
+	}
+}
