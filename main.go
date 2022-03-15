747fa98a74fbf832929a5f4223e7e51087aa95c5
voocel <voocel@gmail.com>
2022-03-15 22:35:24 +0800

    adjust command

--- main.go
+++ main.go
@@ -11,27 +11,40 @@ const (
 )
 
 func main() {
-	app := &cli.App{
-		CustomAppHelpTemplate: Summary(),
-		Commands: []*cli.Command{
-			{
-				Name:   "send",
-				Usage:  "send network",
-				Action: send,
-				Flags: []cli.Flag{
-					&cli.StringFlag{Name: "ip", Usage: "Enter target address", Value: defaultAddr},
-				},
-			},
-			{
-				Name:   "receive",
-				Usage:  "receive network",
-				Action: receive,
+	app := cli.NewApp()
+	app.Usage = "The high-performance file transfer for Golang"
+	app.Version = Version
+	app.EnableBashCompletion = true
+	//app.CustomAppHelpTemplate = Summary()
+
+	app.Action = baseCmd
+	app.Flags = []cli.Flag{}
+	app.Commands = []*cli.Command{
+		{
+			Name:   "send",
+			Usage:  "send network",
+			Action: send,
+			Flags: []cli.Flag{
+				&cli.StringFlag{Name: "ip", Usage: "Enter target address", Value: defaultAddr},
 			},
 		},
-		Flags: []cli.Flag{},
+		{
+			Name:   "receive",
+			Usage:  "receive network",
+			Action: receive,
+		},
 	}
+
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func baseCmd(c *cli.Context) (err error) {
+	if c.NArg() == 0 {
+		print(Summary())
+		return
+	}
+	return
 }
