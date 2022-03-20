df7ed13a55172804b90a05708aa850cdfdcfc090
voocel <voocel@gmail.com>
2022-03-20 21:45:44 +0800

    feature: support interactive and accessible prompts

--- app.go
+++ app.go
@@ -3,6 +3,8 @@ package main
 import (
 	"io"
 	"os"
+
+	"github.com/AlecAivazis/survey/v2"
 )
 
 type app struct {
@@ -27,4 +29,34 @@ func newApp(opts *startOpts) *app {
 	}
 
 	return a
+}
+
+func (a *app) Start() error {
+	for {
+		_, err := a.selectAddr()
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (a *app) selectAddr() (addr string, err error) {
+	var target string
+	addr = a.opts.addr
+	err = survey.AskOne(&survey.Input{
+		Message:  "Choose an address:",
+		Suggest: func(toComplete string) []string {
+			return []string{defaultAddr}
+		},
+	}, &target, survey.WithValidator(survey.Required), surveyIcons())
+	if err != nil {
+		return "", err
+	}
+	return
+}
+
+func surveyIcons() survey.AskOpt {
+	return survey.WithIcons(func(icons *survey.IconSet) {
+		icons.SelectFocus.Text = "→"
+	})
 }
--- go.mod
+++ go.mod
@@ -5,17 +5,8 @@ go 1.17
 require github.com/urfave/cli/v2 v2.3.0
 
 require (
+	github.com/AlecAivazis/survey/v2 v2.3.2
 	github.com/google/uuid v1.3.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
-)
-
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/russross/blackfriday/v2 v2.0.1 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
